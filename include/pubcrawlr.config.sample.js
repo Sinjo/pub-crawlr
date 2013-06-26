594a43445b3e79b55ba78123dcfdf626cf9d45ce
Chris Sinjakli <chris@sinjakli.co.uk>
2013-06-26 22:39:33 +0100

    More whitespace fixing

--- include/pubcrawlr.config.sample.js
+++ include/pubcrawlr.config.sample.js
@@ -1,14 +1,14 @@
 pubcrawlr = {};
 
 pubcrawlr.config = {
-	
+
 	// Settings related to Google API
 	google : {
 		// Your Google OAuth Client ID
 		client_id: "somedigits.apps.googleusercontent.com",
-		
+
 		// The URL to redirect to when the user authorises access
 		oauth_auth_redirect_uri: "http://example.com/"
 	}
-	
+
 };
